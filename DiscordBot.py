9ef32336b61aac3c7c5b6526e68baab44093a261
dstruiksma <detmer56@gmail.com>
2023-02-18 17:14:40 +0100

    stupid pymovie editor library bugfix for pyinstaller

--- DiscordBot.py
+++ DiscordBot.py
@@ -5,5 +5,5 @@ class DiscordBot(discord.Client):
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord!')
         channel = self.get_channel(89017811220844544)
-        with open('gif_test.gif', 'rb') as fp:
+        with open('test.gif', 'rb') as fp:
             await channel.send(file=discord.File(fp, 'test.gif'))
